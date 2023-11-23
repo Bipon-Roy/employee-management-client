2bf566eea52eb3bbb03013163e2d25748d068239
Bipon Roy <biponroy400@gmail.com>
2023-11-23 23:40:08 +0600

    Added firebase setup , login & register page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Login from "../Pages/Shared/Login/Login";
+import SignUp from "../Pages/Shared/SignUp/SIgnUp";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
     },
 ]);

4d2ee7819f44c30e394c7a8d3226a1cb01bdf5f4
Bipon Roy <biponroy400@gmail.com>
2023-11-24 17:56:57 +0600

    Added Register Page functionality

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -4,22 +4,32 @@ import useAuth from "../../hook/useAuth";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { BiLogInCircle } from "react-icons/bi";
+import SocialLogin from "../../Components/SocialLogin";
+import useAxiosPublic from "../../hook/useAxiosPublic";
+import useAxiosSecure from "../../hook/useAxiosSecure";
+
+const image_host_keys = import.meta.env.VITE_IMAGE_HOST_KEY;
+const image_host_api = `https://api.imgbb.com/1/upload?key=${image_host_keys}`;
 
 const SignUp = () => {
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+
     const [showPassword, setShowPassword] = useState(false);
     const { createUser, handleUpdateProfile } = useAuth();
     const route = useNavigate();
 
-    const handleRegister = (e) => {
+    const handleRegister = async (e) => {
         e.preventDefault();
-
         const formData = new FormData(e.currentTarget);
         const name = formData.get("name");
-        const photoURL = formData.get("photoURL");
+        let photoURL = formData.get("photoURL");
         const email = formData.get("email");
         const password = formData.get("password");
-
-        console.log(name, photoURL, email, password);
+        const account_no = formData.get("account_no");
+        const salary = formData.get("salary");
+        const role = formData.get("role");
+        const address = formData.get("address");
 
         if (password.length < 6) {
             Swal.fire({
@@ -43,59 +53,134 @@ const SignUp = () => {
             });
             return;
         }
-        createUser(email, password)
-            .then(() => {
-                handleUpdateProfile(name, photoURL);
-                Swal.fire({
-                    icon: "success",
-                    title: "Welcome!",
-                    text: "Registration Successful!",
+        const imageFile = { image: photoURL };
+        const res = await axiosPublic.post(image_host_api, imageFile, {
+            headers: {
+                "content-type": "multipart/form-data",
+            },
+        });
+        if (res.data.success) {
+            photoURL = res.data.data.display_url;
+            const userInfo = {
+                name,
+                email,
+                account_no,
+                salary,
+                role,
+                address,
+                image: photoURL,
+            };
+            createUser(email, password)
+                .then(() => {
+                    handleUpdateProfile(name, photoURL);
+
+                    axiosPublic.post("/users", userInfo).then((res) => {
+                        if (res.data.insertedId) {
+                            console.log("user added to the database");
+                            formData.reset();
+                            Swal.fire({
+                                icon: "success",
+                                title: "Welcome!",
+                                text: "User created successfully!",
+                            });
+                            route("/");
+                            window.location.reload();
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.error(error);
                 });
-                route("/");
-                window.location.reload();
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        }
     };
     return (
         <div className="max-w-7xl mx-auto">
-            <div className="mx-auto w-[60%] shadow border-2 p-8">
-                <form className="mx-auto space-y-4" onSubmit={handleRegister}>
+            <div className="mx-4 md:mx-auto md:w-[75%] lg:w-1/2 shadow-lg border p-4 md:p-8">
+                <form className=" space-y-4" onSubmit={handleRegister}>
                     <h1 className="font-bold text-lg text-center mb-4 uppercase">Register Here</h1>
-                    <div className="form-control">
-                        <input
-                            name="name"
-                            type="text"
-                            placeholder="Your Name"
-                            className="input input-bordered w-full bg-white"
-                            required
-                        />
-                    </div>
-                    <div className="form-control">
-                        <input
-                            name="email"
-                            type="email"
-                            placeholder="Email"
-                            className="input input-bordered w-full bg-white"
-                            required
-                        />
+                    <div className="flex gap-3 md:gap-6">
+                        <div className="form-control md:w-1/2">
+                            <input
+                                name="name"
+                                type="text"
+                                placeholder="Your Name"
+                                className="input input-bordered w-full focus:outline-none"
+                                required
+                            />
+                        </div>
+                        <div className="form-control md:w-1/2">
+                            <input
+                                name="email"
+                                type="email"
+                                placeholder="Email"
+                                className="input input-bordered w-full focus:outline-none"
+                                required
+                            />
+                        </div>
                     </div>
 
-                    <div className="form-control relative">
+                    <div className="flex gap-3 md:gap-6">
+                        <div className="form-control relative md:w-1/2">
+                            <input
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Password"
+                                className="input input-bordered w-full bg-white focus:outline-none"
+                                required
+                            />
+                            <span
+                                className="text-lg absolute top-3 right-3"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </span>
+                        </div>
+                        <div className="form-control md:w-1/2">
+                            <input
+                                name="account_no"
+                                type="number"
+                                placeholder="Your Bank Account Number"
+                                className="input input-bordered w-full bg-white focus:outline-none"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div className="form-control">
                         <input
-                            name="password"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Password"
-                            className="input input-bordered w-full bg-white"
+                            name="salary"
+                            type="number"
+                            placeholder="Your Salary"
+                            className="input input-bordered w-full  focus:outline-none"
                             required
                         />
-                        <span
-                            className="text-lg absolute top-3 right-3"
-                            onClick={() => setShowPassword(!showPassword)}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-[#22223b] font-semibold">
+                                User Role
+                            </span>
+                        </label>
+                        <select
+                            required
+                            name="role"
+                            className="input input-bordered w-full  focus:outline-none"
                         >
-                            {showPassword ? <FaEyeSlash /> : <FaEye />}
-                        </span>
+                            <option value="" disabled>
+                                Choose User Role
+                            </option>
+                            <option value="Employee">Employee</option>
+                            <option value="HR">HR</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <textarea
+                            rows="4"
+                            name="address"
+                            type="number"
+                            placeholder="Your Address"
+                            className="pt-1 pl-4 rounded-lg border w-full border-[#D2D4D7] focus:outline-none"
+                            required
+                        />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -121,6 +206,7 @@ const SignUp = () => {
                         </Link>
                     </div>
                 </form>
+                <SocialLogin />
             </div>
         </div>
     );

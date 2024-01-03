319c77bc2838829b0af8f1053b1697c638d40c16
Bipon Roy <biponroy400@gmail.com>
2024-01-04 02:33:06 +0600

    Updated Main Layout

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,10 +4,16 @@ import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
-        <div className="">
-            <Navbar />
-            <Outlet />
-            <Footer />
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-none">
+                <Navbar />
+            </div>
+            <div className="flex-grow">
+                <Outlet />
+            </div>
+            <div className="flex-none">
+                <Footer />
+            </div>
         </div>
     );
 };
